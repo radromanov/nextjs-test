8bc0eca14726e2d19d9b97bbbbc3afd25cbe11f0
Radostin Romanov <rad.romanov@gmail.com>
2024-06-01 10:05:53 +0300

    Add hover, focus, active states to card

--- components/Cards/Card.tsx
+++ components/Cards/Card.tsx
@@ -13,11 +13,16 @@ interface Props
 export const Card = ({ title, children, className, ...props }: Props) => {
   return (
     <section
-      className={cn("bg-neutral-100 rounded-md py-3 px-6", className)}
+      className={cn(
+        "group bg-neutral-100 rounded-md py-3 px-6 outline-none -outline-offset-2 hover:outline-blue-500 focus:outline-blue-500 active:outline-blue-500 cursor-pointer",
+        className
+      )}
       {...props}
     >
       <h3>
-        <span className="font-bold">{title}</span>
+        <span className="font-bold group-hover:text-blue-500 group-hover:underline">
+          {title}
+        </span>
       </h3>
 
       {children}
