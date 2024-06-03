ff8f2f3a9490e6bf7cabd13986d2668e85f09b28
Radostin Romanov <rad.romanov@gmail.com>
2024-06-03 10:26:27 +0300

    Updates to font sizes within media breakpoints

--- components/Icons/Icon.tsx
+++ components/Icons/Icon.tsx
@@ -14,7 +14,7 @@ export const Icon = ({ children, className, ...props }: Props) => {
       strokeWidth={1.5}
       stroke="currentColor"
       className={cn(
-        "w-16 mobile-md:w-20 mobile-lg:w-20 mobile-lg:max-w-12 tablet-sm:max-w-full tablet-sm:w-16 laptop:w-24 desktop:w-20 desktop-lg:w-20",
+        "w-16 mobile-md:w-20 mobile-lg:w-20 mobile-lg:max-w-10 tablet-sm:max-w-full tablet-sm:w-16 laptop:w-24 laptop:max-w-11 desktop:max-w-full desktop:w-20 desktop-lg:w-20",
         className
       )}
       {...props}
